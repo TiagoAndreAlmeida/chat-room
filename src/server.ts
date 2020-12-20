2b0d3ead72ded90bab2ae4bf3c5506ebc7f30708
TiagoAndreAlmeida <thyagoeap@gmail.com>
2020-12-20 11:04:47 -0300

    init serve socket

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Socket, Server } from 'socket.io';
 
 const app = express();
+app.use(cors());
+
+const httpServer = createServer(app);
+
 
 app.get('/', (request, response) => {
     return response.json({message: "hello world"});
 });
 
-app.listen(3000);
+const io = new Server(httpServer, {
+    cors: { origin: "*" }
+});
+
+io.on('connection', (socket: Socket) => {
+    console.log("New user connected....");
+});
+
+httpServer.listen(3333);
